85d695ae19ee8fa117af9313f3eb3367dfacff7b
Lucas Severo <lucassalves65@gmail.com>
2016-06-28 20:02:21 -0300

    order by popcon as classmethod, and tests
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>
    Signed-off-by: Lucas Mattioli <lucas.mattioli@live.com>

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -1,5 +1,13 @@
 import unittest
 import pet.classifier
+from pet.classifier import Classifier
+import random
+import popcon
+
+
+class FakePackage():
+    def __init__(self):
+        self.name = ''
 
 
 class TestPetClassifier(unittest.TestCase):
@@ -7,5 +15,19 @@ class TestPetClassifier(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
+    def test_classify_popcon(self):
+
+        package_libdigest_md4_perl = FakePackage()
+        package_libdigest_md4_perl.name = 'libdigest-md4-perl'
+        package_libsharyanto_utils_perl = FakePackage()
+        package_libsharyanto_utils_perl.name = 'libsharyanto-utils-perl'
+
+        packages = [package_libdigest_md4_perl, package_libsharyanto_utils_perl]
+        random.shuffle(packages)
+        ordered_packages = Classifier.order_packages_by_popcon(packages)
+
+        for i in range(0, len(packages) - 1):
+            package1 = popcon.package(ordered_packages[i].name)
+            package2 = popcon.package(ordered_packages[i + 1].name)
+            self.assertGreaterEqual(package1[ordered_packages[i].name],
+                                    package2[ordered_packages[i + 1].name])
